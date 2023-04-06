449b71384077569e698199b4f3aaafe83620bf19
Travis <76796661+traffis@users.noreply.github.com>
2023-04-05 22:53:41 -0400

    Complete Class Synax practice

--- javascript/practice-for-week-04-class-practice/classes/person.js
+++ javascript/practice-for-week-04-class-practice/classes/person.js
@@ -1,4 +1,28 @@
-// Your code here
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  introduce() {
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`);
+  }
+
+  static introducePeople(people) {
+    if (Array.isArray(people) === false) {
+      throw new Error("introducePeople only takes an array as an argument.");
+    }
+
+    for (const person of people) {
+      if (person instanceof Person === false) {
+        throw new Error("All items in array must be Person class instances.");
+      } else {
+        person.introduce();
+      }
+    }
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
