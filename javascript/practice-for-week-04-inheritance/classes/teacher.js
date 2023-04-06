b917e0e38e6dcabfcd3205a8fdbc27034a45af2c
Travis <76796661+traffis@users.noreply.github.com>
2023-04-06 05:05:44 -0400

    Complete Inheritance practice for teacher class

--- javascript/practice-for-week-04-inheritance/classes/teacher.js
+++ javascript/practice-for-week-04-inheritance/classes/teacher.js
@@ -1,6 +1,22 @@
 const Person = require('./person');
 
-// Your code here
+class Teacher extends Person {
+  constructor(firstName, lastName, subject, yearsOfExperience) {
+    super(firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  static combinedYearsOfExperience(teachers) {
+    let totalYearsOfExperience = 0;
+
+    teachers.forEach(teacher => {
+      totalYearsOfExperience += teacher.yearsOfExperience;
+    });
+
+    return totalYearsOfExperience;
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
