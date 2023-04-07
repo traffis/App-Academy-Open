7fe89bdb84f2e421aa12dd2df6f02487ef1043a7
Travis <76796661+traffis@users.noreply.github.com>
2023-04-07 03:54:13 -0400

    Complete Class practice for evil-dragon.js

--- javascript/practice-for-week-04-class-dragons-long-practice/classes/evil-dragon.js
+++ javascript/practice-for-week-04-class-dragons-long-practice/classes/evil-dragon.js
@@ -1,1 +1,28 @@
-// Your code here
+const Dragon = require('./dragon');
+
+class EvilDragon extends Dragon {
+  constructor(name, color, evilDoings, nemesis) {
+    super(name, color);
+    this.evilDoings = evilDoings;
+    this.nemesis = nemesis;
+  }
+
+  dontInviteThemOverForDinner() {
+    this.evilDoings.forEach(evilDoing => {
+      console.log(`${this.name} will ${evilDoing}`)
+    });
+  }
+
+  burnsNemesis() {
+    return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`;
+  }
+}
+
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+  module.exports = EvilDragon;
+} catch {
+  module.exports = null;
+}
