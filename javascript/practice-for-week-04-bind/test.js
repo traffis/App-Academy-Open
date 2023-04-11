63d7c54ea45e00aaa1989177882581fe5b1ce28c
Travis <76796661+traffis@users.noreply.github.com>
2023-04-11 17:29:40 -0400

    Import Employee class and create bound functions to finish practice

--- javascript/practice-for-week-04-bind/test.js
+++ javascript/practice-for-week-04-bind/test.js
@@ -1,1 +1,12 @@
-// Your code here
+const Employee = require('./employee');
+
+// Create new employee and call sayName method after 2 seconds
+let johnwick = new Employee('John Wick', 'Dog Lover');
+let sayNameFunc = johnwick.sayName;
+let sayName = sayNameFunc.bind(johnwick);
+setTimeout(sayName, 2000);
+
+// Call sayOccupation method after 3 seconds
+let sayOccupationFunc = johnwick.sayOccupation;
+let sayOccupation = sayOccupationFunc.bind(johnwick);
+setTimeout(sayOccupation, 3000);
