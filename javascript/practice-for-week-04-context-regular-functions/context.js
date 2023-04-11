b470afaeb9c135f586e77b9b9de1e11a44f34441
Travis <76796661+traffis@users.noreply.github.com>
2023-04-11 03:58:31 -0400

    Add comments for predictions of what happens when each scenario is ran

--- javascript/practice-for-week-04-context-regular-functions/context.js
+++ javascript/practice-for-week-04-context-regular-functions/context.js
@@ -15,23 +15,23 @@ class Fish {
 const nemo = new Fish('Nemo');
 
 /********************************* Scenario 1 *********************************/
-// eat(); // ?
+//eat(); // prints "undefined eats fish food"
 
 
 /********************************* Scenario 2 *********************************/
 // nemo.eat = eat;
-// nemo.eat(); // ?
+// nemo.eat(); // prints "Nemo eats fish food"
 
 
 /********************************* Scenario 3 *********************************/
 // nemo.eat = eat;
-// eat(); // ?
+// eat(); // prints "undefined eats fish food"
 
 
 /********************************* Scenario 4 *********************************/
-// nemo.swim(); // ?
+// nemo.swim(); // prints "Nemo swimming in the water"
 
 
 /********************************* Scenario 5 *********************************/
 // const swim = nemo.swim;
-// swim(); // ?
+// swim(); // throws an error
