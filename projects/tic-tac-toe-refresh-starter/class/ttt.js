186a55471cb9947024de9e20bdfc2e4a370908c0
Travis <76796661+traffis@users.noreply.github.com>
2023-04-13 05:43:24 -0400

    Complete cursor.js and ttt.js to pass tests

--- projects/tic-tac-toe-refresh-starter/class/ttt.js
+++ projects/tic-tac-toe-refresh-starter/class/ttt.js
@@ -29,12 +29,90 @@ class TTT {
   }
 
   static checkWin(grid) {
-
     // Return 'X' if player X wins
     // Return 'O' if player O wins
     // Return 'T' if the game is a tie
     // Return false if the game has not ended
+    const isGridEmpty = grid.every(row => row.every(cell => cell === ' '));
+    const isGridFull = grid.every(row => row.every(cell => cell !== ' '));
 
+    if (isGridEmpty) {
+      return false;
+    }
+
+    if (isGridFull) {
+      return 'T';
+    }
+
+    return (
+      this.horizontalWinCheck(grid) ||
+      this.verticalWinCheck(grid) ||
+      this.diagonalWinCheck(grid)
+    );
+  }
+
+  static horizontalWinCheck(grid) {
+    let winner = grid.find(row =>
+      row.every(cell => cell === row[0] && cell !== ' ')
+    );
+
+    if (winner) {
+      return winner[0];
+    } else {
+      return false;
+    }
+  }
+
+  static verticalWinCheck(grid) {
+    let winner;
+
+    for (let col = 0; col < grid.length; col++) {
+      let count = 1;
+      winner = grid[0][col]
+
+      for (let row = 1; row < grid[col].length; row++) {
+        if (grid[row][col] === winner && grid[row][col] !== ' ') {
+          count++;
+        }
+
+        if (count === grid.length) {
+          return winner;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  static diagonalWinCheck(grid) {
+    let winner = '';
+
+    // forward diagonal check
+    for (let i = 0; i < grid.length; i++) {
+      winner = grid[0][0];
+
+      if (!(grid[i][i] === winner) || winner === ' ') {
+        winner = '';
+        break;
+      }
+    }
+
+    if (winner) return winner;
+
+    // backward diagonal check
+    for (let row = 0; row < grid.length; row++) {
+      let col = grid.length - row - 1;
+      winner = grid[row][col];
+
+      if (!(grid[row][col] === winner) || winner === ' ') {
+        winner = '';
+        break;
+      }
+    }
+
+    if (winner) return winner;
+
+    return false;
   }
 
   static endGame(winner) {
