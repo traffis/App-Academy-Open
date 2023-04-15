da8a36e938925c521b97689d534cc94423995793
Travis <76796661+traffis@users.noreply.github.com>
2023-04-15 06:56:10 -0400

    Complete project; add commands; complete checkWin method with helper functions; create placeMove method

--- projects/connect-four-refresh-starter/class/connect-four.js
+++ projects/connect-four-refresh-starter/class/connect-four.js
@@ -20,25 +20,189 @@ class ConnectFour {
     Screen.initialize(6, 7);
     Screen.setGridlines(true);
 
-    // Replace this with real commands
-    Screen.addCommand('t', 'test command (remove)', ConnectFour.testCommand);
+    // Add commands to play the game
+    Screen.addCommand('left', 'move cursor left', this.cursor.left.bind(this.cursor));
+    Screen.addCommand('right', 'move cursor right', this.cursor.right.bind(this.cursor));
+    Screen.addCommand('space', 'place move', ConnectFour.placeMove.bind(this));
 
     this.cursor.setBackgroundColor();
     Screen.render();
   }
 
-  // Remove this
-  static testCommand() {
-    console.log("TEST COMMAND");
-  }
-
   static checkWin(grid) {
-
     // Return 'X' if player X wins
     // Return 'O' if player O wins
     // Return 'T' if the game is a tie
     // Return false if the game has not ended
+    const isGridEmpty = grid.every(row => row.every(cell => cell === ' '));
+    const isGridFull = grid.every(row => row.every(cell => cell !== ' '));
 
+    if (isGridEmpty) {
+      return false;
+    }
+
+    if (isGridFull) {
+      return 'T'
+    }
+
+    return (
+      this.horizontalWinCheck(grid) ||
+      this.verticalWinCheck(grid) ||
+      this.diagonalWinCheck(grid)
+    );
+  }
+
+  static horizontalWinCheck(grid) {
+    let count = 0;
+    let winner;
+
+    for (let row = 0; row < grid.length; row++) {
+      for (let col = 0; col < grid[row].length - 3; col++) {
+        if (grid[row][col] !== ' ') {
+          winner = grid[row][col];
+          count++;
+
+          for (let i = 1; i <= 3; i++) {
+            if (winner === grid[row][col + i] && grid[row][col + i] !== ' ') {
+              count++;
+            } else {
+              winner = '';
+              count = 0;
+              break;
+            }
+          }
+        }
+
+        if (count === 4) {
+          return winner;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  static verticalWinCheck(grid) {
+    let count = 0;
+    let winner;
+
+    for (let col = 0; col < grid[0].length; col++) {
+      for (let row = 0; row < grid.length - 3; row++) {
+
+        if (grid[row][col] !== ' ') {
+          winner = grid[row][col];
+          count++;
+
+          for (let i = 1; i <=3; i++) {
+            if (winner === grid[row + i][col] && grid[row + i][col] !== ' ') {
+              count++;
+            } else {
+              winner = '';
+              count = 0;
+              break;
+            }
+          }
+        }
+
+        if (count === 4) {
+          return winner;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  static diagonalWinCheck(grid) {
+    let count = 0;
+    let winner;
+
+    // diagonal downward win check
+    for (let row = 0; row < grid.length - 3; row++) {
+      for (let col = 0; col < grid[row].length - 3; col++) {
+        if (grid[row][col] !== ' ') {
+          winner = grid[row][col];
+          count++;
+
+          for (let i = 1; i <= 3; i++) {
+            if (winner === grid[row + i][col + i] && grid[row + i][col + i] !== ' ') {
+              count++;
+            } else {
+              winner = '';
+              count = 0;
+              break;
+            }
+          }
+
+          if (count === 4) {
+            return winner;
+          } else {
+            winner = '';
+            count = 0;
+          }
+        }
+      }
+    }
+
+    // diagonal upward win check
+    for (let row = grid.length - 1; row > 3; row--) {
+      for (let col = 0; col < grid[row].length; col++) {
+        if (grid[row][col] !== ' ') {
+          winner = grid[row][col];
+          count++;
+
+          for (let i = 1; i <= 3; i++) {
+            if (winner === grid[row - i][col + i] && grid[row - i][col + i] !== ' ') {
+              count++;
+            } else {
+              winner = '';
+              count = 0;
+              break;
+            }
+          }
+
+          if (count === 4) {
+            return winner;
+          } else {
+            winner = '';
+            count = 0;
+          }
+        }
+      }
+    }
+
+    return false;
+  }
+
+  static placeMove() {
+    Screen.setMessage('');
+    const col = this.cursor.col;
+    const bottomRow = Screen.grid.length - 1;
+
+    if (Screen.grid[0][col] !== ' ') {
+      Screen.setMessage('This column is full. Please choose another column.');
+      Screen.render();
+    } else {
+      for (let row = bottomRow; row >= 0; row--) {
+        if (Screen.grid[row][col] === ' ') {
+          Screen.setGrid(row, col, this.playerTurn);
+          Screen.render();
+          break;
+        }
+      }
+
+      if (this.playerTurn === 'O') {
+        this.playerTurn = 'X';
+      } else {
+        this.playerTurn = 'O';
+      }
+    }
+
+    const winner = ConnectFour.checkWin(Screen.grid);
+
+    if (winner) {
+      ConnectFour.endGame(winner);
+    }
   }
 
   static endGame(winner) {
