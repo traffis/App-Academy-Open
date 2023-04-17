719f968ce060edce5be2c0c72bb633484786d957
Travis <76796661+traffis@users.noreply.github.com>
2023-04-16 21:08:48 -0400

    Complete Error Handling practice

--- javascript/practice-for-week-04-error-handling/error-handling.js
+++ javascript/practice-for-week-04-error-handling/error-handling.js
@@ -7,20 +7,45 @@ function sum(array) {
   return sum;
 }
 
-let res = sum(null);
-console.log(res);
+try {
+  let res = sum(null);
+  console.log(res);
+} catch (error) {
+  if (error instanceof TypeError) {
+    console.log(error.message);
+  }
+}
+
 
 // 2.
 // tests
-sayName("Alex");
-sayName(1);
-// Your code here
+function sayName(name) {
+  if (typeof name !== 'string') {
+    throw new Error('Invalid name! Must be a string!');
+  } else {
+    console.log(name);
+  }
+}
+
+try {
+  sayName("Alex");
+  sayName(1);
+} catch (error) {
+  console.log(`${error.name}: ${error.message}`);
+}
+
 
 // 3.
 function greet(greeting) {
   if (!greeting) {
-    throw new Error("There was no greeting given.");
+    throw new Error("Hello World!");
   }
 
   console.log(greeting);
+}
+
+try {
+  greet(null);
+} catch (error) {
+  console.log(error.message);
 }
