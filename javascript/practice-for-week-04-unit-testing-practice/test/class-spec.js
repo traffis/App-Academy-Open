f73912305745b3d54f4ef6d8040e0a10d36a90b5
Travis <76796661+traffis@users.noreply.github.com>
2023-04-19 18:03:01 -0400

    Complete Units Test w/ Mocha and Chai practice

--- javascript/practice-for-week-04-unit-testing-practice/test/class-spec.js
+++ javascript/practice-for-week-04-unit-testing-practice/test/class-spec.js
@@ -4,31 +4,41 @@ const expect = chai.expect;
 const { Word } = require("../class");
 
 describe("Word", function () {
+  let word;
+
+  beforeEach(() => {
+    word = new Word('word');
+  })
+
   describe("Word constructor function", function () {
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      expect(word).to.have.property('word');
     });
-  
+
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      expect(word).to.have.property('word', 'word');
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      expect(word.removeVowels()).to.equal('wrd');
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      expect(word.removeConsonants()).to.equal('o');
     });
   });
-  
+
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      expect(word.pigLatin()).to.equal('ordway');
+
+      const era = new Word ('era');
+
+      expect(era.pigLatin()).to.equal('erayay');
     });
   });
 });
