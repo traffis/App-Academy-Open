67f3b2e6c47d0f413157c20c524246ea697a26a3
Travis <76796661+traffis@users.noreply.github.com>
2023-09-11 00:15:01 -0400

    Complete linked-list.js

--- javascript/practice/week-05/practice-for-week-05-linked-list-practice/linked-list.js
+++ javascript/practice/week-05/practice-for-week-05-linked-list-practice/linked-list.js
@@ -7,15 +7,36 @@ class LinkedListNode {
 
 class LinkedList {
   constructor() {
-    // Your code here
+    this.head = null;
+    this.length = 0;
   }
 
   addToHead(val) {
-    // Your code here
+    const newNode = new LinkedListNode(val);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
   }
 
   addToTail(val) {
-    // Your code here
+    const newNode = new LinkedListNode(val);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let currentNode = this.head;
+
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newNode;
+    }
+    this.length++;
   }
 
   // You can use this function to help debug
