f0a2b7f32f5d24b8a523a44d14d592a48ac672bc
Travis <76796661+traffis@users.noreply.github.com>
2023-09-12 18:18:01 -0400

    Complete 01-singly-linked-list.js

--- projects/data-structures-linked-lists-queue-starter/implementation/01-singly-linked-list.js
+++ projects/data-structures-linked-lists-queue-starter/implementation/01-singly-linked-list.js
@@ -12,55 +12,125 @@ class SinglyLinkedList {
         this.length = 0;
     }
 
-    addToHead(val) { 
+    addToHead(val) {
         // Add node of val to head of linked list
+        const newNode = new SinglyLinkedNode(val);
 
-        // Write your hypothesis on the time complexity of this method here
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+
+        // Write your hypothesis on the time complexity of this method here.
+        // I think the time complexity of addToHead() will be O(1) since
+        // we are just assigning a new node at the head of the singly linked list.
+        // We don't need to iterate through the the linked list, or manipulate
+        // the middle of it.
     }
 
     addToTail(val) {
         // There are bugs in this method! Fix them!!!
-        // Write your hypothesis on the time complexity of this method here
+        // Write your hypothesis on the time complexity of this method here.
+        // I think that the time complexity of addToTail() will be O(n) since
+        // we have to iterate through the whole singly linked list to add to the tail.
 
         // Add node of val to tail of linked list
-        let newNode = new SinglyLinkedNode(data);
+        const newNode = new SinglyLinkedNode(val);
 
-        if (!head) {
-            head = newNode;
-            return head;
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let currentNode = this.head;
+
+            while (currentNode.next) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
         }
-
-        let curr = head;
-        while (curr) {
-            curr = current.next;
-        }
-        curr.next = newNode;
-
-        return head;
+        this.length++;
+        return this;
     }
 
     removeFromHead() {
         // Remove node at head
+        if (!this.head) {
+            return undefined;
+        }
 
-        // Write your hypothesis on the time complexity of this method here
+        const returnNode = this.head;
+
+        if (!this.head.next) {
+            this.head = null;
+        } else {
+            this.head = this.head.next;
+        }
+        this.length--;
+        return returnNode;
+
+        // Write your hypothesis on the time complexity of this method here.
+        // I think that the time complexity of removeFromHead() will be O(1)
+        // since we are just manipulating the head of the singly linked list
+        // and not iterating through it.
     }
 
     removeFromTail() {
         // Remove node at tail
+        // 1 -> 2 -> 3 -> null
+        if (!this.head) {
+            return undefined;
+        }
 
-        // Write your hypothesis on the time complexity of this method here
+        let currentNode = this.head;
+        this.length--;
+
+        if (!currentNode.next) {
+            this.head = null;
+            return currentNode;
+        }
+
+        while (currentNode.next.next) {
+            currentNode = currentNode.next;
+        }
+
+        const returnNode = currentNode.next;
+        currentNode.next = null;
+        return returnNode;
+
+        // Write your hypothesis on the time complexity of this method here.
+        // I think the time complexity of removeFromTail() will be O(n) since
+        // we have to iterate through almost the whole singly linked list to
+        // remove the tail node.
     }
 
     peekAtHead() {
         // Return value of head node
+        if (!this.head) {
+            return undefined;
+        }
+        return this.head.value;
 
-        // Write your hypothesis on the time complexity of this method here
+        // Write your hypothesis on the time complexity of this method here.
+        // The time complexity of peekAtHead() will be O(1) since we don't
+        // have to iterate through the singly linked list to grab the head's
+        // value.
     }
 
     print() {
         // Print out the linked list
-        
-        // Write your hypothesis on the time complexity of this method here
+        let currentNode = this.head;
+
+        while (currentNode) {
+            console.log(currentNode.value);
+            currentNode = currentNode.next;
+        }
+
+        // Write your hypothesis on the time complexity of this method here.
+        // The time complexity of print() will be O(n) because we iterate
+        // through the whole singly linked list to print it.
     }
 }
 
