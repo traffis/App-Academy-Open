a367fe2415d7a132c60f61cb3666ac36994b3ccd
Travis <76796661+traffis@users.noreply.github.com>
2023-09-12 21:55:47 -0400

    Complete 02-doubly-linked-list.js

--- projects/data-structures-linked-lists-queue-starter/implementation/02-doubly-linked-list.js
+++ projects/data-structures-linked-lists-queue-starter/implementation/02-doubly-linked-list.js
@@ -16,13 +16,15 @@ class DoublyLinkedList {
 
     addToHead(val) {
         // There are bugs in this method! Fix them!!!
-        // Write your hypothesis on the time complexity of this method here
+        // Write your hypothesis on the time complexity of this method here.
+        // This would be O(1) because we're only manipulating the head of
+        // the doubly linked list.
 
         // Add node of val to head of linked list
-        let newNode = new DoublyLinkedNode(val);
+        const newNode = new DoublyLinkedNode(val);
 
-        if (this.length >= 0) {
-            this.head.previous = newNode;
+        if (this.length > 0) {
+            this.head.prev = newNode;
             newNode.next = this.head;
             this.head = newNode;
         } else {
@@ -35,32 +37,94 @@ class DoublyLinkedList {
 
     addToTail(val) {
         // Add node of val to tail of linked list
+        const newNode = new DoublyLinkedNode(val);
 
-        // Write your hypothesis on the time complexity of this method here
+        if (this.length > 0) {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        } else {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+
+        this.length++;
+
+        // Write your hypothesis on the time complexity of this method here.
+        // This would be O(1) because we're only manipulating the tail of
+        // the doubly linked list.
     }
 
     removeFromHead() {
         // Remove node at head
+        if (!this.head) {
+            return undefined;
+        }
 
-        // Write your hypothesis on the time complexity of this method here
+        const returnNode = this.head;
+
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = returnNode.next;
+            this.head.prev = null;
+        }
+
+        this.length--;
+        return returnNode.value;
+
+        // Write your hypothesis on the time complexity of this method here.
+        // The time complexity of this would be O(1) because we don't iterate
+        // through the doubly linked list.
     }
 
     removeFromTail() {
         // Remove node at tail
+        if (!this.head) {
+            return undefined;
+        }
 
-        // Write your hypothesis on the time complexity of this method here
+        const returnNode = this.tail;
+
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = this.tail.prev;
+            this.tail.next= null;
+        }
+
+        this.length--;
+        return returnNode.value;
+
+        // Write your hypothesis on the time complexity of this method here.
+        // The time complexity of this would be O(1) because we don't iterate
+        // though the doubly linked list.
     }
 
     peekAtHead() {
         // Return value of head node
+        if (!this.head) {
+            return undefined;
+        }
+        return this.head.value;
 
-        // Write your hypothesis on the time complexity of this method here
+        // Write your hypothesis on the time complexity of this method here.
+        // The time complexity of this would be O(1) because we just grab
+        // the doubly linked list's head value without iterating through it.
     }
 
     peekAtTail() {
         // Return value of tail node
-        
-        // Write your hypothesis on the time complexity of this method here
+        if (!this.head) {
+            return undefined;
+        }
+        return this.tail.value;
+
+        // Write your hypothesis on the time complexity of this method here.
+        // The time complexity of this would be O(1) because we just grab
+        // the doubly linked lists' head value without iterating through it.
     }
 }
 
