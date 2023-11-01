34150a7992d65b5aa615c16ad376b0265cf34f48
Travis <76796661+traffis@users.noreply.github.com>
2023-11-01 04:56:02 -0400

    Complete hash table practice

--- javascript/practice/week-05/practice-for-week-05-hash-tables-practice/hash-table.js
+++ javascript/practice/week-05/practice-for-week-05-hash-tables-practice/hash-table.js
@@ -11,29 +11,66 @@ class KeyValuePair {
 class HashTable {
 
   constructor(numBuckets = 4) {
-    // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
-    // Your code here
+    return parseInt(sha256(key).slice(0, 8), 16);
   }
 
   hashMod(key) {
-    // Your code here
+    return this.hash(key) % this.capacity;
   }
 
   insertNoCollisions(key, value) {
-    // Your code here
+    const hash = this.hashMod(key);
+    const keyValuePair = new KeyValuePair(key, value);
+
+    if (this.data[hash] !== null) {
+      throw new Error("hash collision or same key/value pair already exists!");
+    }
+
+    this.data[hash] = keyValuePair;
+    this.count++;
   }
 
   insertWithHashCollisions(key, value) {
-    // Your code here
+    const hash = this.hashMod(key);
+    const keyValuePair = new KeyValuePair(key, value);
+
+    if (this.data[hash] === null) {
+      this.data[hash] = keyValuePair;
+    } else {
+      keyValuePair.next = this.data[hash];
+      this.data[hash] = keyValuePair;
+    }
+
+    this.count++;
   }
 
   insert(key, value) {
-    // Your code here
-  }
+    const hash = this.hashMod(key);
+    const keyValuePair = new KeyValuePair(key, value);
 
+    if (this.data[hash] === null) {
+      this.data[hash] = keyValuePair;
+      this.count++;
+    } else {
+      let currentNode = this.data[hash];
+
+      while (currentNode.next !== null && currentNode.key !== keyValuePair.key) {
+        currentNode = currentNode.next;
+      }
+
+      if (currentNode.key === keyValuePair.key) {
+        currentNode.value = keyValuePair.value;
+      } else {
+        this.insertWithHashCollisions(key, value);
+      }
+    }
+  }
 }
 
 
