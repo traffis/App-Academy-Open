1f5202f267b823fba496919bdc31231ce660d297
Travis <76796661+traffis@users.noreply.github.com>
2023-11-02 05:57:42 -0400

    Complete 01-implementation.js

--- javascript/long-practice/week-05/practice-for-week-05-hash-tables-long-practice/phases/01-implementation.js
+++ javascript/long-practice/week-05/practice-for-week-05-hash-tables-long-practice/phases/01-implementation.js
@@ -9,8 +9,9 @@ class KeyValuePair {
 class HashTable { // get O(1), set O(1), deleteKey O(1)
 
   constructor(numBuckets = 8) {
-    // Initialize your buckets here
-    // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
@@ -30,22 +31,101 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
 
   insert(key, value) {
-    // Your code here
+    const hash = this.hashMod(key);
+    const keyValuePair = new KeyValuePair(key, value);
+
+    if (this.data[hash] === null) {
+      this.data[hash] = keyValuePair;
+    } else {
+      let currentNode = this.data[hash];
+
+      while (currentNode.next !== null && currentNode.key !== keyValuePair.key) {
+        currentNode = currentNode.next;
+      }
+
+      if (currentNode.key === keyValuePair.key) {
+        currentNode.value = keyValuePair.value;
+      } else {
+        keyValuePair.next = this.data[hash];
+        this.data[hash] = keyValuePair;
+      }
+    }
+
+    this.count++;
   }
 
 
   read(key) {
-    // Your code here
+    const hash = this.hashMod(key);
+    let currentNode = this.data[hash];
+
+    while (currentNode) {
+      if (currentNode.key === key) {
+        return currentNode.value;
+      }
+      currentNode = currentNode.next;
+    }
+
+    return undefined;
   }
 
 
   resize() {
-    // Your code here
+    this.capacity *= 2;
+    const newData = new Array(this.capacity).fill(null);
+
+    for (let i = 0; i < this.data.length; i++) {
+      let currentNode = this.data[i];
+
+      while (currentNode) {
+        const hash = this.hashMod(currentNode.key);
+
+        if (newData[hash] === null) {
+          newData[hash] = currentNode;
+        } else {
+          currentNode.next = newData[hash];
+        }
+
+        currentNode = currentNode.next;
+      }
+    }
+
+    this.data = newData;
   }
 
 
   delete(key) {
-    // Your code here
+    const hash = this.hashMod(key);
+    let currentNode = this.data[hash];
+
+    if (!this.read(key)) {
+      return "Key not found"
+    }
+
+    if (currentNode.key === key && currentNode.next === null) {
+      this.data[hash] = null;
+      this.count--;
+    } else {
+      let prevNode = currentNode;
+      currentNode = currentNode.next;
+
+      if (prevNode.key === key) {
+        this.data[hash] = currentNode;
+        this.count--;
+        return prevNode;
+      }
+
+      while (currentNode) {
+        if (currentNode.key === key) {
+          prevNode.next = currentNode.next;
+          this.count--;
+          return currentNode;
+        }
+
+        prevNode = currentNode;
+        currentNode = currentNode.next;
+      }
+    }
   }
 }
 
