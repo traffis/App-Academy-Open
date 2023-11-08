f0f5b9cf48a96a60109cc676f744d3df3dbed139
Travis <76796661+traffis@users.noreply.github.com>
2023-11-07 20:45:11 -0500

    Complete selection sort practice

--- javascript/practice/week-06/practice-selection-sort/selection-sort.js
+++ javascript/practice/week-06/practice-selection-sort/selection-sort.js
@@ -1,22 +1,30 @@
-
-
 function selectionSort(arr) {
 
   // Copy the original array
+  let arrCopy = arr.slice();
 
   // Create an array to store the sorted values
+  let sorted = [];
 
   // While the array is not empty...
+  while (arrCopy.length > 0) {
 
     // Do not move this console.log
     console.log(sorted.join(","));
 
     // Find the index of the minimum value in the unsorted half
+    let minIdx = 0;
+    for (let i = 0; i < arrCopy.length; i++) {
+      minIdx = arrCopy[i] < arrCopy[minIdx] ? i : minIdx;
+    }
 
     // Save and remove the value at the min index
+    let value = arrCopy.splice(minIdx, 1)[0];
 
     // Add the min value to the end of the sorted array
-
+    sorted.push(value);
+  }
+  return sorted;
 }
 
 
@@ -24,22 +32,29 @@ function selectionSort(arr) {
 function selectionSortInPlace(arr) {
 
   // Set a pointer at zero diving the array into sorted and unsorted halves
+  let divider = 0;
 
   // Repeat while the unsorted half is not empty:
+  while (divider < arr.length) {
 
     // Do not move this console.log
     console.log(arr.join(","));
 
     // Find the index of the minimum value in the unsorted half
+    let minIdx = divider;
+    for (let i = divider; i < arr.length; i++) {
+      minIdx = arr[i] < arr[minIdx] ? i : minIdx;
+    }
 
-    // Save the min value
-
-    // Shift every unsorted value to the left of the min value to the right by 1
-
-    // Put the min value at the divider
+    // Shift every unsorted value to the left of the min value to the right by 1,
+    // and put min value at divider
+    for (let i = minIdx; i > divider; i--) {
+      [arr[i], arr[i - 1]] = [arr[i - 1], arr[i]];
+    }
 
     // Increment the divider and repeat
-
+    divider++;
+  }
 }
 
 
