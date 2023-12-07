4f816354ecb52f1e1d9dcab480a42f45a08b9d91
Travis <76796661+traffis@users.noreply.github.com>
2023-12-06 20:56:17 -0500

    Complete tree practice

--- javascript/long-practice/week-06/practice-for-week-06-binary-search-tree-long-practice/tree-practice.js
+++ javascript/long-practice/week-06/practice-for-week-06-binary-search-tree-long-practice/tree-practice.js
@@ -5,63 +5,191 @@ const { BinarySearchTree, TreeNode } = require('./binary-search-tree.js');
 // Practice problems on binary trees
 
 function findMinBST (rootNode) {
-  // Your code here
+  let minNode = rootNode;
+  let currentNodeLeft = rootNode.left;
+
+  while (currentNodeLeft) {
+    if (currentNodeLeft.val < minNode.val) minNode = currentNodeLeft;
+    currentNodeLeft = currentNodeLeft.left;
+  }
+
+  return minNode.val;
 }
 
 function findMaxBST (rootNode) {
-  // Your code here
+  let maxNode = rootNode;
+  let currentNodeRight = rootNode.right;
+
+  while (currentNodeRight) {
+    if (currentNodeRight.val > maxNode.val) maxNode = currentNodeRight;
+    currentNodeRight = currentNodeRight.right;
+  }
+
+  return maxNode.val;
 }
 
 function findMinBT (rootNode) {
-  // Your code here
+  if (rootNode === null) return;
+
+  let minValue = rootNode.val;
+  let leftMinValue = findMinBT(rootNode.left);
+  let rightMinValue = findMinBT(rootNode.right);
+
+  if (leftMinValue < minValue) minValue = leftMinValue;
+  if (rightMinValue < minValue) minValue = rightMinValue;
+
+  return minValue;
 }
 
 function findMaxBT (rootNode) {
-  // Your code here
+  if (rootNode === null) return;
+
+  let maxValue = rootNode.val;
+  let leftMaxValue = findMaxBT(rootNode.left);
+  let rightMaxValue = findMaxBT(rootNode.right);
+
+  if (leftMaxValue > maxValue) maxValue = leftMaxValue;
+  if (rightMaxValue > maxValue) maxValue = rightMaxValue;
+
+  return maxValue;
 }
 
 function getHeight (rootNode) {
-  // Your code here
-}
+  if (rootNode === null) return -1;
 
-function balancedTree (rootNode) {
-  // Your code here
+  let leftHeight = getHeight(rootNode.left);
+  let rightHeight = getHeight(rootNode.right);
+
+  return 1 + Math.max(leftHeight, rightHeight);
 }
 
 function countNodes (rootNode) {
-  // Your code here
+  if (rootNode === null) return 0;
+  return 1 + countNodes(rootNode.left) + countNodes(rootNode.right);
+}
+
+function balancedTree (rootNode) {
+  const queue = [rootNode];
+
+  while (queue.length > 0) {
+    let node = queue.shift();
+
+    if (node.left) queue.push(node.left);
+    if (node.right) queue.push(node.right);
+
+    let diff = Math.abs(getHeight(node.left) - getHeight(node.right));
+    if (diff > 1) return false;
+  }
+
+  return true;
 }
 
 function getParentNode (rootNode, target) {
-  // Your code here
+  if (rootNode.val === target) return null;
+
+  const queue = [rootNode]
+
+  while (queue.length > 0) {
+    let removed = queue.shift();
+
+    if (removed.left) {
+      if (removed.left.val === target) return removed;
+      queue.push(removed.left);
+    }
+
+    if (removed.right) {
+      if (removed.right.val === target) return removed;
+      queue.push(removed.right);
+    }
+  }
+
+  return undefined;
 }
 
 function inOrderPredecessor (rootNode, target) {
-  // Your code here
+  if (!rootNode) return null;
+
+  const stack = [];
+  let currentNode = rootNode;
+  let prevNode = null;
+
+  while (true) {
+    if (currentNode !== null) {
+      stack.push(currentNode);
+      currentNode = currentNode.left;
+    } else if (stack.length > 0) {
+      currentNode = stack.pop();
+
+      if (target === currentNode.val && prevNode === null) return null;
+      if (target === currentNode.val) return prevNode.val;
+
+      prevNode = currentNode;
+      currentNode = currentNode.right;
+    } else {
+      break;
+    }
+  }
 }
 
 function deleteNodeBST(rootNode, target) {
   // Do a traversal to find the node. Keep track of the parent
+  let node, parent;
+  let found = findNode(rootNode, target);
 
   // Undefined if the target cannot be found
-
   // Set target based on parent
+  if (found) {
+    node = found[0];
+    parent = found[1];
+  } else {
+    return undefined
+  }
 
-  // Case 0: Zero children and no parent:
-  //   return null
+  if (node){
+    if (!node.left && !node.right && !parent) {
+      // Case 0: Zero children and no parent:
+      // return null
 
-  // Case 1: Zero children:
-  //   Set the parent that points to it to null
+      return null;
+    } else if (!node.left && !node.right && parent) {
+      // Case 1: Zero children:
+      //   Set the parent that points to it to null
 
-  // Case 2: Two children:
-  //  Set the value to its in-order predecessor, then delete the predecessor
-  //  Replace target node with the left most child on its right side, 
-  //  or the right most child on its left side.
-  //  Then delete the child that it was replaced with.
+      parent.val > node.val ? parent.left = null : parent.right = null;
+    } else if (node.left && node.right) {
+      // Case 2: Two children:
+      //  Set the value to its in-order predecessor, then delete the predecessor
+      //  Replace target node with the left most child on its right side,
+      //  or the right most child on its left side.
+      //  Then delete the child that it was replaced with.
 
-  // Case 3: One child:
-  //   Make the parent point to the child
+      let val = inOrderPredecessor(rootNode, target);
+      deleteNodeBST(rootNode, val);
+      node.val = val;
+    } else if (node.left && !node.right) {
+      // Case 3: One child:
+      // Make the parent point to the child
 
+      parent.val > node.val ? parent.left = node.left : parent.right = node.left;
+    } else if (!node.left && node.right) {
+      // Still Case 3
+
+      parent.val > node.val ? parent.left = node.right : parent.right = node.right;
+    }
+  }
+
+}
+
+const findNode = function (rootNode, val, prev = null) {
+  if (!rootNode) {
+    return null;
+  } else if (rootNode.val === val) {
+    return [rootNode, prev];
+  } else if (rootNode.val > val) {
+    return findNode(rootNode.left, val, rootNode);
+  } else {
+    return findNode(rootNode.right, val, rootNode);
+  }
 }
 
 module.exports = {
