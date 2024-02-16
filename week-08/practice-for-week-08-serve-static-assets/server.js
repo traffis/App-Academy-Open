bc27e3ed373d7b50d0bafd348797d330ca35210f
Travis <76796661+traffis@users.noreply.github.com>
2024-02-16 05:46:12 -0500

    Complete Serve Static Assets practice

--- week-08/practice-for-week-08-serve-static-assets/server.js
+++ week-08/practice-for-week-08-serve-static-assets/server.js
@@ -2,7 +2,37 @@ const http = require('http');
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  // Your code here
+  const assetsDir = './assets';
+
+  if (req.method === 'GET' && req.url.startsWith('/static')) {
+    const urlArray = req.url.split("/");
+
+    if (urlArray.length >= 3) {
+      const fileName = urlArray[urlArray.length - 1];
+      const fileExtension = fileName.slice(fileName.indexOf('.') + 1);
+      const assetType = urlArray[2];
+      const assetPath = `${assetsDir}/${assetType}/${fileName}`;
+      let asset;
+
+      if (fileExtension === 'jpg') {
+        res.setHeader('Content-Type', 'image/jpeg');
+        asset = fs.readFileSync(assetPath);
+      }
+
+      if (fileExtension === 'css') {
+        res.setHeader('Content-Type', 'text/css');
+        asset = fs.readFileSync(assetPath, 'utf-8');
+      }
+
+      res.statusCode = 200;
+      return res.end(asset);
+    }
+  }
+
+  const htmlFile = fs.readFileSync('./index.html', 'utf-8');
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  return res.end(htmlFile);
 });
 
 const port = 5000;
